c4de7194603692271ca2461cef12e65ed395bd13
Abdulhammed <70025758+abdulhammed24@users.noreply.github.com>
2021-10-17 13:39:08 +0100

    Add files via upload

---
+++ cta.js
@@ -0,0 +1,28 @@
+const ctaEl = document.querySelector(".cta");
+
+ctaEl.innerHTML = `
+
+<div class="section-container">
+            <img src="https://images.ctfassets.net/88a6qmzs2wdz/2e2TsIsGLaRNV65XbCPjNo/38bdefa478997663bad0f94e1be40529/Artboard_Copy.png?h=345&q=90&w=170"
+                alt="cta-img" class="cta-img">
+            <div class="cta-content">
+                <div class="cta-title">
+                    Join thousands of small business owners who trust Bench with their books
+                </div>
+                <p>See what running a business is like with Bench on your side. Try us for free—we’ll do one prior month
+                    of your
+                    bookkeeping and prepare a set of financial statements for you to keep.
+                </p>
+
+                <a class="cta-btn section-btn" href="">Start a Free Trail
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="297 387.5 18 17" fill="#fff">
+                        <path
+                            d="M305 398.4a1.09 1.09 0 0 0 0 1.5 1.08 1.08 0 0 0 .7.3c.3 0 .5-.1.7-.3l3.2-3.2v-1.4s0-.1-.1-.1l-3.1-3.1a1.09 1.09 0 0 0-1.5 0 1.09 1.09 0 0 0 0 1.5l1.3 1.3H298c-.6 0-1 .5-1 1 0 .6.5 1 1 1h8.3l-1.3 1.5zm1.5-10.9c-2.8 0-5.5 1.4-7.1 3.8-.3.5-.2 1.1.3 1.4s1.1.2 1.4-.3c1.2-1.8 3.2-2.8 5.3-2.8 3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4c-2.1 0-4.1-1.1-5.3-2.8-.3-.5-1-.6-1.4-.3-.5.3-.6 1-.3 1.4 1.6 2.3 4.2 3.7 7 3.7 4.7 0 8.5-3.8 8.5-8.5.1-4.6-3.7-8.4-8.4-8.4z">
+                        </path>
+                    </svg>
+                </a>
+            </div>
+            <img src="https://images.ctfassets.net/88a6qmzs2wdz/5BGb5uU22ZJAh7y63UZDvH/4109e19e0f4ba46d442ff27ed5c8fa2f/Artboard.png?h=345&q=90&w=170"
+                alt="cta-img" class="cta-img">
+        </div>
+`;
---
+++ header.js
@@ -0,0 +1,54 @@
+const headerEl = document.querySelector(".header");
+
+headerEl.innerHTML = `
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="header-container section-container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="https://pkf-ng.com/content/images/logo.png" alt="" class="navbar-logo">
+                </a>
+
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#main_nav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> Company </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Products </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Services </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#"> Submenu item 1</a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 2 </a></li>
+                                <li><a class="dropdown-item" href="#"> Submenu item 3 </a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item"><a href="" class="nav-link">Client Login</a></li>
+
+                        <li class="nav-item"><a href="" class="nav-link">Request Demo</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+`;
