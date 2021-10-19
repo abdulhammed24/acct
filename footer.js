835a9aca40cc9cca2fa31e26fb7647858f055b7f
Abdulhammed <70025758+abdulhammed24@users.noreply.github.com>
2021-10-19 13:40:18 +0100

    Add files via upload

--- footer.js
+++ footer.js
@@ -31,7 +31,7 @@ footerEl.innerHTML = `
                         <h3 class="title">Quick Link</h3>
                         <ul>
                             <li><a href="about.html">About Us</a></li>
-                            <li><a href="#">Services</a></li>
+                            <li><a onclick="window.location.href='about.html#service'">Services</a></li>
                             <li><a href="#">Tokens</a></li>
                             <li><a href="#">Team</a></li>
                             <li><a href="contact.html">Contact</a></li>
